e8c0ed4cd641ab3268b680a9f7efe32ae548691d
Shubham <byshubham6@gmail.com>
2024-05-22 11:07:42 +0530

    #

--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html')
+
+def blogs(request):
+    return render(request, 'blog/blogs.html')
+
+def contact(request):
+    return render(request, 'blog/contact.html')
